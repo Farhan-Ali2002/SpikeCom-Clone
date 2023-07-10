f08638cd36fa00d6a18c141ed8637a2ad619d26b
Farhan Ali <farhanali0006@gmail.com>
2023-07-10 20:22:29 +0500

    changes

--- src/components/c_info_card_list.js
+++ src/components/c_info_card_list.js
@@ -1,6 +1,20 @@
 import React from "react";
 
 function CinfCardList(params) {
+    let cardBData = [
+        {logo:icon1, desc:"Quick Market helps create an amazing on-demand grocery experience. One of the world's largest and fastest growing online food delivery and quick commerce platform",num1:"+2.5M",num2:"+8M",num3:"+85K",num4:"+7.5M"}
+       ,{logo: icon2, desc:"One of the largest Supermarket in Middle East & North African regions. It is considered to be top player in 'Brick & Click' E-Commerce channels. It has a growing MOM associated with no less than 95% SL",num1:"+1.9M",num2:"+17M",num3:"+600K",num4:"+15M"}
+       ,{logo:icon3, desc:"First Grocery Applicaiton in Kingdom of Saudi Arabia. Danube is one of the first retailer in the Kingdom of Saudi Arabia. It provides wide range of high-end assortment of products with high average basket size",num1:"+1.5M",num2:"+15M",num3:"+165K",num4:"+4M"},
+        {logo:icon4, desc:"Clicflyer is the highest rated & frequently used app in its category. It is making weekly savings easy for it's valuable customers. It offers features including browsing flyers, create shopping list & find your faviorite offers",num1:"+127M",num2:"+15M",num3:"+872K",num4:"+6.5M"},
+        {logo:icon5, desc:"Jahez in less than two years came on top of food on demands business in Kingdom Saudi Arabia. It serves more than 20,000 restaurants for 4.4 million users each month",num1:"+40M",num2:"+8M",num3:"+4.4M",num4:"+14M"}
+       ,{logo:icon6, desc:"Hunger Station helps create an amazing on-demand grocery experience. One of the world's largest and fastest growing online food delivery and quick commerce platform",num1:"+2.5M",num2:"+8M",num3:"+85K",num4:"+7.5M"},
+       {logo:icon6, desc:"LuLu has always been known as a trend-setter for the retail industry in the region. LuLu targets users from the asian sub-continent by providing imported products to it's customers",num1:"+250K",num2:"+12M",num3:"+100K",num4:"+1.5M"},
+       {logo:icon6, desc:"With PandaClick, the largest grocery chain in Kingdom of Saudi Arabia is a click away. It is massively growing by leveraging on the high traffic of offline stores & converting them to online users across their 200+ stores",num1:"+1M",num2:"+12M",num3:"+150K",num4:"+2.6M"},
+       {logo:icon6, desc:"Ranked #1 in regions app store has experienced growth, popularity in United Arab Emmirates, Kingdom of Saudi Arabia and Egypt. Stay home and order and we will deliver straight to your door step",num1:"+11.3M",num2:"+18M",num3:"+8M",num4:"+20M"},
+       {logo:icon6, desc:"Zad delivery groceries to your door step. Zad came to market to fulfill the aggressive demand on e-grocers especially in Center region and considered one of the top #10 App in Saudi Arabia.",num1:"+1M",num2:"+12M",num3:"",num4:""},
+       {logo:icon6, desc:"Jumlaty app is focused on an economy scale business model where you can buy day to day commodities in bulk with slashing prices. World's largest and fastest growing online food delivery and quick commerce platform.",num1:"",num2:"",num3:"",num4:""},
+       {logo:icon6, desc:"Jumlaty app is focused on an economy scale business model where you can buy day to day commodities in bulk with slashing prices. World's largest and fastest growing online food delivery and quick commerce platform.",num1:"",num2:"",num3:"",num4:""},
+    ]
     return(
         <div></div>
     )
